b315a0fd3c3ec05c3ec7dd21073115838ad55bb2
Stefano Fancello <gentoo.stefano@gmail.com>
2024-05-20 08:55:48 +0200

    use stuff chain for summarizing

--- src/ytsummary.py
+++ src/ytsummary.py
@@ -22,3 +22,24 @@ def get_summary(url):
 	summary = chain.invoke(chunks)
 
 	return summary['output_text']
+
+def get_summary2(url):
+	loader = YoutubeLoader.from_youtube_url(url)
+	documents = loader.load()
+	llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+
+	# Split the document into chunks
+	from langchain.text_splitter import RecursiveCharacterTextSplitter
+	text_splitter = RecursiveCharacterTextSplitter(
+		chunk_size=2000,
+		chunk_overlap=0
+	)
+
+	chain = load_summarize_chain(llm, chain_type="stuff")
+	summary = []
+	for document in documents:
+		chunk = (text_splitter.create_documents([document.page_content], [document.metadata]))
+		summary.append(chain.invoke(chunk))
+	return "\n".join([s['output_text'] for s in summary])
+
+
